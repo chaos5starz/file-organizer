e63e891d3a6d7d8c9e9f732fdc479228bbd97d78
wessam mohamed <chaos5starz@gmail.com>
2025-11-03 00:05:26 +0200

    added a dictionary for files and their extensions to be more dynamic

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
 import os
 import shutil
 
-text_files =[]
-pdf_files = []
+
+dict_items = {"images" : [".jpg" , ".png"] ,
+              "text files" : [".txt"] , 
+              "pdf files" : [".pdf"] ,
+              "word files" : [".docx"]
+                }
 
 path_main = input("give me a folder path to organize ")
 
 
-name_sub_text = "text files"
-path_sub_text = os.path.join(path_main , name_sub_text)
+for folder , extension in dict_items.items() :
+    if not os.path.exists(os.path.join(path_main , folder)) :
+        os.mkdir(os.path.join(path_main , folder))
+            
 
-name_sub_pdf = "pdf files"
-path_sub_pdf = os.path.join(path_main , name_sub_pdf)
-
-if any(f.endswith(".txt") for f in os.listdir(path_main)):
-    if not os.path.exists(path_sub_text):
-        os.mkdir(path_sub_text)
-
-if any(f.endswith(".pdf") for f in os.listdir(path_main)):
-    if not os.path.exists(path_sub_pdf):
-        os.mkdir(path_sub_pdf)
-
-
-for f in os.listdir(path_main):
-    if f.endswith(".txt") :
-        source = os.path.join(path_main, f)
-        destination = path_sub_text
-        shutil.move(source , destination)
-        text_files.append(f)
-    elif f.endswith(".pdf") :
-        source = os.path.join(path_main, f)
-        destination = path_sub_pdf
-        shutil.move(source , destination)
-        pdf_files.append(f)
-
+for item in os.listdir(path_main) :
+    for folder , extension in dict_items.items() :
+        if item.endswith(tuple(extension)) :
+            source = os.path.join(path_main , item)
+            destination = os.path.join(path_main , folder)
+            shutil.move(source , destination)
+        
 
