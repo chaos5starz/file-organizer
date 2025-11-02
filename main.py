32ffd114ae5aa986e50dae129f9dddae4f5ec4cc
wessam mohamed <chaos5starz@gmail.com>
2025-11-03 01:01:24 +0200

    added the 'other' folder for files with extension not in dictionary

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import shutil
 dict_items = {"images" : [".jpg" , ".png"] ,
               "text files" : [".txt"] , 
               "pdf files" : [".pdf"] ,
-              "word files" : [".docx"]
-                }
+              "word files" : [".docx"],
+               "other" : [] }
 
 path_main = input("give me a folder path to organize ")
 
@@ -17,10 +17,21 @@ for folder , extension in dict_items.items() :
             
 
 for item in os.listdir(path_main) :
+    flag = False
     for folder , extension in dict_items.items() :
-        if item.endswith(tuple(extension)) :
+        if not os.path.isdir(os.path.join(path_main , item)) :
+            if item.endswith(tuple(extension)) :
+                source = os.path.join(path_main , item)
+                destination = os.path.join(path_main , folder)
+                shutil.move(source , destination)
+                flag = True
+    if not os.path.isdir(os.path.join(path_main , item)) :
+        if  flag == False:
             source = os.path.join(path_main , item)
-            destination = os.path.join(path_main , folder)
+            destination = os.path.join(path_main , "other")
             shutil.move(source , destination)
+
+print ("done")
+
         
 
