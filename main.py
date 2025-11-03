463015c868b620f8a72c2fed23c1b5a15fc07b16
wessam mohamed <chaos5starz@gmail.com>
2025-11-03 19:32:42 +0200

    cleaner code with no flags

--- main.py
+++ main.py
@@ -15,21 +15,23 @@ for folder , extension in dict_items.items() :
     if not os.path.exists(os.path.join(path_main , folder)) :
         os.mkdir(os.path.join(path_main , folder))
             
-
 for item in os.listdir(path_main) :
-    flag = False
-    for folder , extension in dict_items.items() :
-        if not os.path.isdir(os.path.join(path_main , item)) :
+    if not os.path.isdir(os.path.join(path_main , item)) :
+        for folder , extension in dict_items.items() :
             if item.endswith(tuple(extension)) :
                 source = os.path.join(path_main , item)
                 destination = os.path.join(path_main , folder)
                 shutil.move(source , destination)
-                flag = True
-    if not os.path.isdir(os.path.join(path_main , item)) :
-        if  flag == False:
+                break
+                
+        else :
             source = os.path.join(path_main , item)
             destination = os.path.join(path_main , "other")
             shutil.move(source , destination)
+
+
+
+
 
 print ("done")
 
